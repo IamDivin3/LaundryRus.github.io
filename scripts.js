4fe9439f3889ace5dd949c467439cd94f1dc85c7
unknown <agudosi91@gmail.com>
2023-08-07 23:08:22 +0100

    adds all files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function redirectToServices() {
+    // Redirect to services
+    window.location.href = '/.services.html'
+}
+
+
+function redirectContact() {
+    // Redirect to services
+    window.location.href = '/.contacts.html'
+}
